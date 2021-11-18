cea4c3e55e83146e067144ed3edb377fd25a908a
Javier Lete <javier@eskura21.com>
2021-11-18 12:51:36 +0100

    Documentación básica de Hola Mundo

--- HolaMundo/src/com/ipartek/formacion/mf0967/uf2216/Hola.java
+++ HolaMundo/src/com/ipartek/formacion/mf0967/uf2216/Hola.java
@@ -3,8 +3,26 @@ package com.ipartek.formacion.mf0967.uf2216;
 public class Hola {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hola a todos");
 	}
-
 }
+
+//java Hola -> JRE hace: Hola.main(new String[] {})
+
+//package -> La "agrupación" (paquete) de clases la definimos con package
+
+//public -> La clase se puede acceder desde cualquier clase
+//class -> Define una nueva clase
+//HolaMundo -> Nombre que nos inventamos para la clase
+
+//public -> Se permite llamar al método desde cualquier clase
+//static -> Se va a llamar al método desde el nombre de la propia clase
+//void -> La ejecución del método no devuelve ningún resultado (es como un procedimiento)
+//main() -> Método que se busca desde el JRE cuando vamos a ejecutar un programa Java desde consola
+
+//String[] -> Array de String
+//args -> argumentos recibidos por consola
+
+//System -> Clase de la API de java que representa el sistema operativo sobre el que se está ejecutando la aplicación (pertenece al paquete java.lang)
+//out -> Variable estática, representa la salida de consola
+//println() -> Método, manda una línea a la consola
