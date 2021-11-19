a7a7d6bbc26f74e35eae4f40b2de453f15b4f8dc
Javier Lete <javier@eskura21.com>
2021-11-19 13:25:05 +0100

    Operadores

--- HolaMundo/src/com/ipartek/formacion/mf0967/uf2216/Sintaxis.java
+++ HolaMundo/src/com/ipartek/formacion/mf0967/uf2216/Sintaxis.java
@@ -42,12 +42,12 @@ public class Sintaxis {
 		char c = '\n'; // 'g'; \n, \t, \r...
 
 		boolean bl = true;
-		
+
 		b = 127;
 		System.out.println(b);
 		b++;
 		System.out.println(b);
-		
+
 //		for(b = 1; b < 300; b++) {
 //			System.out.println(b);
 //		}
@@ -65,16 +65,16 @@ public class Sintaxis {
 
 		Character ce = null;
 		Boolean ble = null;
-		
+
 		// Cadena de texto
 		String nombre1 = new String("Javier");
 		String nombre2 = new String("Javier");
 
 		System.out.println(nombre1 == nombre2);
 		System.out.println(nombre1.equals(nombre2));
-		
+
 		// Métodos de String
-		
+
 		System.out.println("Este es un texto".toUpperCase());
 
 		StringBuffer log = new StringBuffer();
@@ -89,33 +89,33 @@ public class Sintaxis {
 		// Conversiones de String a otro tipo
 		String texto = "5";
 		int enteroPrimitivo = Integer.parseInt(texto);
-		
+
 		// Conversiones de otro tipo a String
 		enteroPrimitivo = 5;
 		texto = String.valueOf(enteroPrimitivo);
-		
+
 		Integer enteroEncapsulado = 5;
 		texto = enteroEncapsulado.toString();
-		
+
 		// Fechas
 		// Java 1.0
-		Date fecha10 = new Date(); //new Date(2021 - 1900, 11 - 1, 19, 20, 29, 00);
+		Date fecha10 = new Date(); // new Date(2021 - 1900, 11 - 1, 19, 20, 29, 00);
 		System.out.println(fecha10.toLocaleString());
-		
+
 		// Java 1.1
 		Calendar cal = new GregorianCalendar();
 		DateFormat formato = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
 		System.out.println(formato.format(cal.getTime()));
-		
+
 		// Java 1.8 (java.time)
 		LocalDateTime ldt = LocalDateTime.now();
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
 		System.out.println(ldt.format(dtf));
-		
+
 		String fechaTexto = "18-12-2020 17:18:19";
-		ldt =  LocalDateTime.parse(fechaTexto, dtf);
+		ldt = LocalDateTime.parse(fechaTexto, dtf);
 		System.out.println(ldt);
-		
+
 		// Arrays ó Arreglos
 		// Conjunto de elementos de tamaño predefinido
 		// El objeto de tipo array NUNCA cambia de tamaño una vez establecido
@@ -123,30 +123,30 @@ public class Sintaxis {
 		// Todos los elementos son contiguos
 		int tam = 2;
 		int[] arr = new int[tam];
-		
+
 		arr[0] = 7;
 		arr[1] = 5;
 		// arr[2] = 8; //
-		
+
 		// arr.length = 6; //
-		
-		for(int indice = 0; indice < arr.length; indice++) {
+
+		for (int indice = 0; indice < arr.length; indice++) {
 			System.out.println(arr[indice]);
 		}
-		
+
 		char[][] tablero = new char[8][8];
-		
+
 		tablero[0][0] = 't';
 		tablero[0][1] = 'c';
 		tablero[1][0] = 'p';
-		
+
 		tablero[6][0] = 'P';
 		tablero[6][1] = 'P';
 		tablero[7][0] = 'T';
-		
-		for(int x = 0; x < tablero.length; x++) {
+
+		for (int x = 0; x < tablero.length; x++) {
 			for (int y = 0; y < tablero[x].length; y++) {
-				if(tablero[x][y] == '\0') {
+				if (tablero[x][y] == '\0') {
 					System.out.print('.');
 				} else {
 					System.out.print(tablero[x][y]);
@@ -154,30 +154,65 @@ public class Sintaxis {
 			}
 			System.out.println();
 		}
-		
+
 		// Colecciones (mutables)
 		// Java 5 <Tipo>
 		// Java 8 <>
 		// Java 5 for
-		
+
 		ArrayList<Integer> al = new ArrayList<>();
-		
+
 		al.add(5);
 		al.add(7);
 		al.add(10);
-		
-		for(Integer dato: al) {
+
+		for (Integer dato : al) {
 			System.out.println(dato);
 		}
-		
+
 		System.out.println(al.get(1));
-		
+
 		TreeMap<String, String> diccionario = new TreeMap<>();
-		
+
 		diccionario.put("casa", "house");
 		diccionario.put("coche", "car");
-		
+
 		System.out.println(diccionario.get("coche"));
+
+		// Operadores
+		System.out.println(6 % 4);
+
+		int x = 2;
+		System.out.println(1 + x + 10);
+
+		System.out.println(1 <= x & x <= 10);
+		System.out.println(1 <= x && x <= 10);
+
+		System.out.println(6 & 3); // 110 & 011 = 010
+		System.out.println(6 | 3); // 110 | 011 = 111
+		System.out.println(6 ^ 3); // 110 ^ 011 = 101
+		System.out.println(~6);    //~00000110 = 11111001
+		
+		System.out.println(-40 >>> 3); // 00101000 >> 3 = 00000101
+		System.out.println(-40 >> 3);  // 00101000 >> 3 = 00000101
+		System.out.println(-10 << 3);  // 00001010 << 3 = 01010000
+		
+		int y, z;
+		System.out.println(y = z = 3);
+		
+		System.out.println(y++);
+		System.out.println(++z);
+		
+		int a = 5, mayor;
+		b = 20;
+		
+		mayor = a > b ? a : b;
+		
+		System.out.println(mayor);
+		
+		boolean encendido = true;
+		
+		System.out.println(encendido ? "Encendido" : "Apagado");
 	}
 
 }
