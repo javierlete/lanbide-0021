0418ae6f06b8eac703ec30d2fdc865a3b39b4d25
Javier Lete <javier@eskura21.com>
2022-01-03 10:02:18 +0100

    Corrección de nombres de servlets

--- uf2218/src/main/java/com/ipartek/formacion/uf2218/controladores/admin/UsuarioAdminServlet.java
+++ uf2218/src/main/java/com/ipartek/formacion/uf2218/controladores/admin/UsuarioAdminServlet.java
@@ -1,24 +1,21 @@
 package com.ipartek.formacion.uf2218.controladores.admin;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.ipartek.formacion.uf2218.accesodatos.DaoUsuario;
-import com.ipartek.formacion.uf2218.accesodatos.DaoUsuarioMemoria;
-
-@WebServlet("/admin/usuarios")
+@WebServlet("/admin/usuario")
 public class UsuarioAdminServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		DaoUsuario dao = DaoUsuarioMemoria.getInstancia();
+		// DaoUsuario dao = DaoUsuarioMemoria.getInstancia();
 		
-		request.setAttribute("usuarios", dao.obtenerTodos());
-		request.getRequestDispatcher("/WEB-INF/vistas/admin/usuarios.jsp").forward(request, response);
+		request.getRequestDispatcher("/WEB-INF/vistas/admin/usuario.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- uf2218/src/main/java/com/ipartek/formacion/uf2218/controladores/admin/UsuariosAdminServlet.java
+++ uf2218/src/main/java/com/ipartek/formacion/uf2218/controladores/admin/UsuariosAdminServlet.java
@@ -1,21 +1,24 @@
 package com.ipartek.formacion.uf2218.controladores.admin;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/admin/usuario")
+import com.ipartek.formacion.uf2218.accesodatos.DaoUsuario;
+import com.ipartek.formacion.uf2218.accesodatos.DaoUsuarioMemoria;
+
+@WebServlet("/admin/usuarios")
 public class UsuariosAdminServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// DaoUsuario dao = DaoUsuarioMemoria.getInstancia();
+		DaoUsuario dao = DaoUsuarioMemoria.getInstancia();
 		
-		request.getRequestDispatcher("/WEB-INF/vistas/admin/usuario.jsp").forward(request, response);
+		request.setAttribute("usuarios", dao.obtenerTodos());
+		request.getRequestDispatcher("/WEB-INF/vistas/admin/usuarios.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
