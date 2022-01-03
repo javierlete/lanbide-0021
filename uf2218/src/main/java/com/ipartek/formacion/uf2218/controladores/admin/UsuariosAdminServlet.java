4fe957aef60dc9ce6f6591b082f4c8bc6fd04295
Javier Lete <javier@eskura21.com>
2022-01-03 09:28:34 +0100

    Maqueta de administración de usuarios

---
+++ uf2218/src/main/java/com/ipartek/formacion/uf2218/controladores/admin/UsuariosAdminServlet.java
@@ -0,0 +1,22 @@
+package com.ipartek.formacion.uf2218.controladores.admin;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/admin/usuarios")
+public class UsuariosAdminServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/vistas/admin/usuarios.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ uf2218/src/main/webapp/WEB-INF/vistas/admin/usuarios.jsp
@@ -0,0 +1,35 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Mantenimiento de usuarios</title>
+</head>
+<body>
+	<table>
+		<thead>
+			<tr>
+				<th>Id</th>
+				<th>Nombre</th>
+				<th>Email</th>
+				<th>Contraseña</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<th>1</th>
+				<td>Juan González</td>
+				<td>juan@gonzalez.net</td>
+				<td>juanes</td>
+			</tr>
+			<tr>
+				<th>2</th>
+				<td>Pedro Gómez</td>
+				<td>pedro@gomez.net</td>
+				<td>pedro</td>
+			</tr>
+		</tbody>
+	</table>
+</body>
+</html>
