3c41f25e3287fa7a214f2ca4d2331e9ae280e602
Javier Lete <javier@eskura21.com>
2022-02-01 10:22:37 +0100

    Uso del DAO para rellenar la pantalla

--- Ipartekify/src/main/java/com/ipartek/formacion/ipartekify/controlador/IndexServlet.java
+++ Ipartekify/src/main/java/com/ipartek/formacion/ipartekify/controlador/IndexServlet.java
@@ -1,22 +1,44 @@
 package com.ipartek.formacion.ipartekify.controlador;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.ipartek.formacion.ipartekify.dal.DaoArtista;
+import com.ipartek.formacion.ipartekify.dal.FabricaDao;
+import com.ipartek.formacion.ipartekify.dal.FabricaDaoImpl;
+import com.ipartek.formacion.ipartekify.modelos.Artista;
+
 @WebServlet("/index")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		FabricaDao fabrica = new FabricaDaoImpl();
+		DaoArtista dao = fabrica.getArtista();
+		
+		String id = request.getParameter("artista");
+		
+		if(id != null && id.trim().length() > 0) {
+			Artista artista = dao.obtenerPorId(Long.parseLong(id));
+			
+			request.setAttribute("artista", artista);
+		}
+		
+		Iterable<Artista> artistas = dao.obtenerTodos();
+
+		request.setAttribute("artistas", artistas);
+
 		request.getRequestDispatcher("/WEB-INF/vistas/index.jsp").forward(request, response);
 	}
 
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
