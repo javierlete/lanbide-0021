70a040c3db51c6d7546c39f353adc7c6bc408826
Javier Lete <javier@eskura21.com>
2022-02-04 10:10:05 +0100

    Refactorización de IndexServlet

--- Ipartekify/src/main/java/com/ipartek/formacion/ipartekify/controlador/IndexServlet.java
+++ Ipartekify/src/main/java/com/ipartek/formacion/ipartekify/controlador/IndexServlet.java
@@ -23,51 +23,38 @@ import com.ipartek.formacion.ipartekify.modelos.Usuario;
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	private static final FabricaDao fabrica = new FabricaDaoImpl();
+	
+	private static final DaoArtista daoArtista = fabrica.getArtista();
+	private static final DaoAlbum daoAlbum = fabrica.getAlbum();
+	private static final DaoCancion daoCancion = fabrica.getCancion();
+	private static final DaoUsuario daoUsuario = fabrica.getUsuario();
+	
+	private HttpServletRequest request;
+	private Usuario usuario;
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		FabricaDao fabrica = new FabricaDaoImpl();
-		DaoArtista daoArtista = fabrica.getArtista();
-		DaoAlbum daoAlbum = fabrica.getAlbum();
-		DaoCancion daoCancion = fabrica.getCancion();
-		DaoUsuario daoUsuario = fabrica.getUsuario();
-		
 		String strIdArtista = request.getParameter("artista");
 		String strIdAlbum = request.getParameter("album");
 		String strIdCancion = request.getParameter("cancion");
 		
 		String favorito = request.getParameter("favorito");
 		
-		Usuario usuario = (Usuario)request.getSession().getAttribute("usuario");
+		this.usuario = (Usuario)request.getSession().getAttribute("usuario");
+		
+		this.request = request;
 		
 		if(strIdArtista != null && strIdArtista.trim().length() > 0) {
-			long idArtista = Long.parseLong(strIdArtista);
-			Artista artista = daoArtista.obtenerPorId(idArtista);
-			
-			request.setAttribute("artista", artista);
-			
-			Iterable<Album> albumes = daoAlbum.obtenerTodos(idArtista);
-			
-			request.setAttribute("albumes", albumes);
+			artista(strIdArtista);
 		}
 		
 		if(strIdCancion != null && strIdCancion.trim().length() > 0) {
-			long idCancion = Long.parseLong(strIdCancion);
-			Cancion cancion = daoCancion.obtenerPorId(idCancion);
-			
-			request.setAttribute("cancion", cancion);
-			
-			strIdAlbum = cancion.getAlbum().getId().toString();
-			
-			if(favorito != null) {
-				daoUsuario.favoritoCancion(usuario.getId(), idCancion);
-			}
+			strIdAlbum = cancion(strIdCancion, favorito);
 		}
 		
 		if(strIdAlbum != null && strIdAlbum.trim().length() > 0) {
-			long idAlbum = Long.parseLong(strIdAlbum);
-			Album album = daoAlbum.obtenerPorId(usuario.getId(), idAlbum);
-			
-			request.setAttribute("album", album);
+			album(strIdAlbum);
 		}
 		
 		Iterable<Artista> artistas = daoArtista.obtenerTodos();
@@ -75,6 +62,37 @@ public class IndexServlet extends HttpServlet {
 		request.setAttribute("artistas", artistas);
 
 		request.getRequestDispatcher("/WEB-INF/vistas/index.jsp").forward(request, response);
+	}
+
+	private void album(String strIdAlbum) {
+		long idAlbum = Long.parseLong(strIdAlbum);
+		Album album = daoAlbum.obtenerPorId(usuario.getId(), idAlbum);
+		
+		request.setAttribute("album", album);
+	}
+
+	private String cancion(String strIdCancion, String favorito) {
+		long idCancion = Long.parseLong(strIdCancion);
+		Cancion cancion = daoCancion.obtenerPorId(idCancion);
+		
+		request.setAttribute("cancion", cancion);
+		
+		if(favorito != null) {
+			daoUsuario.favoritoCancion(usuario.getId(), idCancion);
+		}
+		
+		return cancion.getAlbum().getId().toString();
+	}
+
+	private void artista(String strIdArtista) {
+		long idArtista = Long.parseLong(strIdArtista);
+		Artista artista = daoArtista.obtenerPorId(idArtista);
+		
+		request.setAttribute("artista", artista);
+		
+		Iterable<Album> albumes = daoAlbum.obtenerTodos(idArtista);
+		
+		request.setAttribute("albumes", albumes);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
