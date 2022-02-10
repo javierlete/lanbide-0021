e685cefe08919b1760aa446001972d3d1e357717
Javier Lete <javier@eskura21.com>
2022-02-10 10:41:37 +0100

    Actualización de la base de datos

--- Ipartekify/bdd/ipartekify.sql
+++ Ipartekify/bdd/ipartekify.sql
@@ -33,7 +33,7 @@ CREATE TABLE `albumes` (
   PRIMARY KEY (`id`),
   KEY `fk_albumes_artistas1_idx` (`artistas_id`),
   CONSTRAINT `fk_albumes_artistas1` FOREIGN KEY (`artistas_id`) REFERENCES `artistas` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,7 +42,7 @@ CREATE TABLE `albumes` (
 
 LOCK TABLES `albumes` WRITE;
 /*!40000 ALTER TABLE `albumes` DISABLE KEYS */;
-INSERT INTO `albumes` VALUES (1,'Sweet Movimiento',2013,'https://i.ytimg.com/vi/1NRQD9ds7ZQ/maxresdefault.jpg',1),(2,'Relayer',1974,'https://i.ytimg.com/vi/YXnj64o198A/mqdefault.jpg',3),(3,'Milliontown',2006,'https://m.media-amazon.com/images/I/61YEkcgK8nL._SY355_.jpg',2),(4,'Looking For La Fiesta',2013,'https://img.discogs.com/E20VSDgPrIJN-XCOQNSKILCcawY=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-5752937-1401702925-7304.jpeg.jpg',1);
+INSERT INTO `albumes` VALUES (2,'Relayer',1974,'https://i.ytimg.com/vi/YXnj64o198A/mqdefault.jpg',3),(4,'Looking For La Fiesta',2013,'https://img.discogs.com/E20VSDgPrIJN-XCOQNSKILCcawY=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-5752937-1401702925-7304.jpeg.jpg',1),(5,'Milliontown',2006,'libros.png',4),(6,'Falling Satellites (Deluxe)',2016,NULL,4),(7,'Sweet Movimiento',2013,NULL,1);
 /*!40000 ALTER TABLE `albumes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -70,7 +70,7 @@ CREATE TABLE `albumes_favoritos` (
 
 LOCK TABLES `albumes_favoritos` WRITE;
 /*!40000 ALTER TABLE `albumes_favoritos` DISABLE KEYS */;
-INSERT INTO `albumes_favoritos` VALUES (2,1),(2,2),(1,3);
+INSERT INTO `albumes_favoritos` VALUES (2,2);
 /*!40000 ALTER TABLE `albumes_favoritos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -86,7 +86,7 @@ CREATE TABLE `artistas` (
   `nombre` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
   `informacion` text COLLATE utf8mb4_general_ci,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -95,7 +95,7 @@ CREATE TABLE `artistas` (
 
 LOCK TABLES `artistas` WRITE;
 /*!40000 ALTER TABLE `artistas` DISABLE KEYS */;
-INSERT INTO `artistas` VALUES (1,'Fundación Tony Manero','Disco Funk'),(2,'Frost*','Rock Progresivo'),(3,'Yes','Rock Progresivo');
+INSERT INTO `artistas` VALUES (1,'Fundación Tony Manero','Disco Funk'),(3,'Yes','Rock Progresivo'),(4,'Frost*','Rock Progresivo muy molón y moderno');
 /*!40000 ALTER TABLE `artistas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -123,7 +123,7 @@ CREATE TABLE `artistas_favoritos` (
 
 LOCK TABLES `artistas_favoritos` WRITE;
 /*!40000 ALTER TABLE `artistas_favoritos` DISABLE KEYS */;
-INSERT INTO `artistas_favoritos` VALUES (1,1),(1,2),(2,3);
+INSERT INTO `artistas_favoritos` VALUES (1,1),(2,3);
 /*!40000 ALTER TABLE `artistas_favoritos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -152,7 +152,7 @@ CREATE TABLE `canciones` (
 
 LOCK TABLES `canciones` WRITE;
 /*!40000 ALTER TABLE `canciones` DISABLE KEYS */;
-INSERT INTO `canciones` VALUES (1,'Fonky Macarrón','00:03:03','K53xne6iK4s',1),(2,'The Gates of Delirium','00:21:16','EdmUAsU2eXI',2),(3,'Hyperventilate','00:07:31','Ol592sakmZU',3),(4,'Soon','00:04:06','QukX9I6ZLsM',2),(5,'Supersexy Girl','00:03:52','PS5zshNRvLo',4);
+INSERT INTO `canciones` VALUES (2,'The Gates of Delirium','00:21:16','EdmUAsU2eXI',2),(4,'Soon','00:04:06','QukX9I6ZLsM',2),(5,'Supersexy Girl','00:03:52','PS5zshNRvLo',4);
 /*!40000 ALTER TABLE `canciones` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -180,7 +180,7 @@ CREATE TABLE `canciones_favoritas` (
 
 LOCK TABLES `canciones_favoritas` WRITE;
 /*!40000 ALTER TABLE `canciones_favoritas` DISABLE KEYS */;
-INSERT INTO `canciones_favoritas` VALUES (1,1),(1,2),(3,2),(2,3),(1,4);
+INSERT INTO `canciones_favoritas` VALUES (1,2),(3,2),(1,4);
 /*!40000 ALTER TABLE `canciones_favoritas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -236,7 +236,7 @@ CREATE TABLE `listas_has_canciones` (
 
 LOCK TABLES `listas_has_canciones` WRITE;
 /*!40000 ALTER TABLE `listas_has_canciones` DISABLE KEYS */;
-INSERT INTO `listas_has_canciones` VALUES (2,1),(3,1),(5,1),(1,2),(1,3),(3,3),(5,5);
+INSERT INTO `listas_has_canciones` VALUES (1,2),(5,5);
 /*!40000 ALTER TABLE `listas_has_canciones` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -274,6 +274,84 @@ UNLOCK TABLES;
 --
 -- Dumping routines for database 'ipartekify'
 --
+/*!50003 DROP FUNCTION IF EXISTS `url_foto` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` FUNCTION `url_foto`(foto VARCHAR(255), id BIGINT) RETURNS varchar(255) CHARSET utf8mb4 COLLATE utf8mb4_general_ci
+    NO SQL
+BEGIN
+RETURN IFNULL(foto, CONCAT('fotos/', id, '.jpg'));
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `albumes_borrar` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `albumes_borrar`(_id BIGINT)
+BEGIN
+DELETE FROM albumes WHERE id=_id;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `albumes_insertar` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `albumes_insertar`(_nombre VARCHAR(45), _anno YEAR, _foto VARCHAR(255), _artistas_id BIGINT)
+BEGIN
+INSERT INTO albumes (nombre, anno, foto, artistas_id) VALUES (_nombre, _anno, _foto, _artistas_id);
+SELECT LAST_INSERT_ID();
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `albumes_modificar` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `albumes_modificar`(_id BIGINT, _nombre VARCHAR(45), _anno YEAR, _foto VARCHAR(255), _artistas_id BIGINT)
+BEGIN
+UPDATE albumes SET nombre = _nombre, anno = _anno, foto = _foto, artistas_id = _artistas_id WHERE id = _id;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `albumes_select_artista` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -286,7 +364,7 @@ UNLOCK TABLES;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `albumes_select_artista`(_id_artista BIGINT)
 BEGIN
-SELECT id, nombre, anno, foto FROM albumes WHERE artistas_id = _id_artista;
+SELECT id, nombre, anno, url_foto(foto, id) AS foto FROM albumes WHERE artistas_id = _id_artista;
 END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -305,7 +383,65 @@ DELIMITER ;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `albumes_select_id`(_id BIGINT)
 BEGIN
-SELECT id, nombre, anno, foto FROM albumes WHERE id = _id;
+SELECT id, nombre, anno, url_foto(foto, id) AS foto, artistas_id FROM albumes WHERE id = _id;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `artistas_delete` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `artistas_delete`(_id BIGINT)
+BEGIN
+DELETE FROM artistas WHERE id = _id;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `artistas_insertar` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `artistas_insertar`(_nombre VARCHAR(45), _informacion TEXT)
+BEGIN
+INSERT INTO artistas (nombre, informacion) VALUES (_nombre, _informacion);
+SELECT LAST_INSERT_ID();
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `artistas_modificar` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `artistas_modificar`(_nombre VARCHAR(45), _informacion TEXT, _id BIGINT)
+BEGIN
+UPDATE artistas SET nombre=_nombre, informacion=_informacion WHERE id=_id;
 END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -600,4 +736,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-02-08 10:26:21
+-- Dump completed on 2022-02-10 10:41:09
