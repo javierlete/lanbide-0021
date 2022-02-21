4b0f8fb4bc076407d2df88446c555a6f22a4da54
Javier Lete <javier@eskura21.com>
2022-02-21 10:19:50 +0100

    Hibernate con anotaciones

--- uf2215/src/main/java/com/ipartek/formacion/uf2215/entidades/Cliente.java
+++ uf2215/src/main/java/com/ipartek/formacion/uf2215/entidades/Cliente.java
@@ -1,11 +1,23 @@
 package com.ipartek.formacion.uf2215.entidades;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name="clientes")
 @Data @AllArgsConstructor @NoArgsConstructor
 public class Cliente {
+	@Id
+	@GeneratedValue(generator="increment")
+	@GenericGenerator(name="increment", strategy = "increment")
 	private Long id;
 	private String nombre;
 	private String apellido;
