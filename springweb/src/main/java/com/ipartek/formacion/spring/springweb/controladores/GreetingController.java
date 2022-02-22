301ba2c51084bfbdfeed524bda2f9530a227b043
Javier Lete <javier@eskura21.com>
2022-02-22 10:45:20 +0100

    Ejemplo Spring MVC

---
+++ springweb/src/main/java/com/ipartek/formacion/spring/springweb/controladores/GreetingController.java
@@ -0,0 +1,15 @@
+package com.ipartek.formacion.spring.springweb.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(required = false, defaultValue = "profe") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+}
