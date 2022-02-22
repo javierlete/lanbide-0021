5fea89300a3763069061243908bb405e4f085df5
Javier Lete <javier@eskura21.com>
2022-02-22 10:45:04 +0100

    Inicio springweb (Spring Web, Dev Tools, Thymeleaf)

---
+++ springweb/src/test/java/com/ipartek/formacion/spring/springweb/SpringwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.springweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
