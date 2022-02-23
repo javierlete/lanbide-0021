cc9d3e3d30d7f60a156c7e3f8ed134534c06b506
Javier Lete <javier@eskura21.com>
2022-02-23 10:38:37 +0100

    Spring Data REST repositories

--- springweb/src/main/java/com/ipartek/formacion/spring/springweb/repositorios/UsuarioRepository.java
+++ springweb/src/main/java/com/ipartek/formacion/spring/springweb/repositorios/UsuarioRepository.java
@@ -1,10 +1,12 @@
 package com.ipartek.formacion.spring.springweb.repositorios;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.ipartek.formacion.spring.springweb.entidades.Usuario;
 
-public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
+@RepositoryRestResource(collectionResourceRel = "usuarios", path = "usuarios")
+public interface UsuarioRepository extends PagingAndSortingRepository<Usuario, Long> {
 	Usuario findByEmail(String email);
 	Iterable<Usuario> findByRol(String rol);
 }
