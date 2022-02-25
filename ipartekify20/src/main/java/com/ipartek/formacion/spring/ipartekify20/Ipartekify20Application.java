7f04f2a3b7c2d609e56769f1684c4641b9b879ef
Javier Lete <javier@eskura21.com>
2022-02-25 09:44:18 +0100

    Creación de proyecto Ipartekify20

---
+++ ipartekify20/src/main/java/com/ipartek/formacion/spring/ipartekify20/Ipartekify20Application.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.ipartekify20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ipartekify20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ipartekify20Application.class, args);
+	}
+
+}
---
+++ ipartekify20/src/test/java/com/ipartek/formacion/spring/ipartekify20/Ipartekify20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.ipartekify20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ipartekify20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
