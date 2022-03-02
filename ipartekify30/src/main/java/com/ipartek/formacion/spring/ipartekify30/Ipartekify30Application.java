21cbc22bd7714122d780a754aabe52eb0034c7bc
Javier Lete <javier@eskura21.com>
2022-03-02 09:15:20 +0100

    Inicio ipartekify30

---
+++ ipartekify30/src/main/java/com/ipartek/formacion/spring/ipartekify30/Ipartekify30Application.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.ipartekify30;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ipartekify30Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ipartekify30Application.class, args);
+	}
+
+}
---
+++ ipartekify30/src/test/java/com/ipartek/formacion/spring/ipartekify30/Ipartekify30ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.ipartekify30;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ipartekify30ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
