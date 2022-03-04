d211d61f66410f35c9b5af7ad3c329113832d997
Javier Lete <javier@eskura21.com>
2022-03-04 09:48:45 +0100

    EntidadesException con constructores explícitos sin Lombok

--- ipartekify30/src/main/java/com/ipartek/formacion/spring/ipartekify30/entidades/EntidadesException.java
+++ ipartekify30/src/main/java/com/ipartek/formacion/spring/ipartekify30/entidades/EntidadesException.java
@@ -1,9 +1,27 @@
 package com.ipartek.formacion.spring.ipartekify30.entidades;
 
-import lombok.experimental.StandardException;
-
-@StandardException
+// @StandardException
 public class EntidadesException extends RuntimeException {
+
+	public EntidadesException() {
+		super();
+	}
+
+	public EntidadesException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public EntidadesException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public EntidadesException(String message) {
+		super(message);
+	}
+
+	public EntidadesException(Throwable cause) {
+		super(cause);
+	}
 
 	private static final long serialVersionUID = 5804973342838400993L;
 
