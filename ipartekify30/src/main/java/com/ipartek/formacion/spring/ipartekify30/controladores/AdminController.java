6d4d2635f1c6ad1d9b6304f600c42a34693e09e2
Javier Lete <javier@eskura21.com>
2022-03-08 10:30:23 +0100

    Plantilla de administración

---
+++ ipartekify30/src/main/java/com/ipartek/formacion/spring/ipartekify30/controladores/AdminController.java
@@ -0,0 +1,14 @@
+package com.ipartek.formacion.spring.ipartekify30.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+	@GetMapping
+	public String index() {
+		return "admin";
+	}
+}
