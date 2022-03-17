96fe0d7eb9bf544032b45edfc7190d19ec11e330
Javier Lete <javier@eskura21.com>
2022-03-17 08:28:34 +0100

    Corrección de UF2215

--- uf2215/src/main/java/com/ipartek/formacion/uf2215/entidades/Cliente.java
+++ uf2215/src/main/java/com/ipartek/formacion/uf2215/entidades/Cliente.java
@@ -2,10 +2,9 @@ package com.ipartek.formacion.uf2215.entidades;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
-import org.hibernate.annotations.GenericGenerator;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,8 +15,7 @@ import lombok.NoArgsConstructor;
 @Data @AllArgsConstructor @NoArgsConstructor
 public class Cliente {
 	@Id
-	@GeneratedValue(generator="increment")
-	@GenericGenerator(name="increment", strategy = "increment")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nombre;
 	private String apellido;
